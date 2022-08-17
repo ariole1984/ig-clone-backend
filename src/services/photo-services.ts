9bf76095f736abe62b80f6b9fa6e471b5e187d48
Ariole Dieujuste <arioledieujuste.com>
2022-08-17 10:47:19 -0400

    REfactored and modularized

---
+++ src/services/photo-services.ts
@@ -0,0 +1,18 @@
+import { db } from '../db/db';
+import { Photo } from "../models/photo";
+
+interface PhotoServices {
+    updateLikes(id:string, inc:number): Promise<Photo>;
+    createPhoto(photo:Photo): Promise<string>;
+    createComment (id:string, comment:string): Promise<Photo>;
+    getAllPhotos():Promise<Photo[]>;
+}
+
+const photosCollection = db.collection<Photo>('photos');
+
+export const getAllPhotos = async (): Promise<Photo[]> => {
+const photos = await photosCollection.find().toArray()
+
+return photos;
+
+};
